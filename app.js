3223298611a8389ee0a32ce2842e04e93460a787
iqbalcb88 <iqbal.cb88@gmail.com>
2022-08-05 10:58:30 +0600

    js functionality done

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
 // console.log(smallCups);
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlighted(idx));
 });
 
 function highlighted(id) {
-  console.log(id);
+  // console.log(id);
   if (
     (smallCups[id].classList.contains('full') &&
       smallCups[id].nextElementSibling === null) ||
@@ -21,4 +24,26 @@ function highlighted(id) {
       cup.classList.remove('full');
     }
   });
+  updateBigCup();
 }
+
+const updateBigCup = () => {
+  const fullCups = document.querySelectorAll('.cup.cup-small.full');
+  // console.log(fullCups.length);
+
+  if (fullCups.length === 0) {
+    liters.innerText = 2000 / 1000 + 'L';
+    percentage.style.visibility = 'hidden';
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(330 * fullCups.length) / smallCups.length}px`;
+    percentage.innerText = `${(100 * fullCups.length) / smallCups.length}%`;
+    liters.innerText = `${((2000 - 250 * fullCups.length) / 1000).toFixed(2)}L`;
+  }
+  if (fullCups.length === smallCups.length) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+  }
+};
